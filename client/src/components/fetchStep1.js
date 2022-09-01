1e30625fd3357f7271b075931ad69c7cb3a81f22
Hamish Starling <hamishstarling@hotmail.co.uk>
2022-09-01 12:51:57 +0100

    Make hover effect on iframe page

--- client/src/components/fetchStep1.js
+++ client/src/components/fetchStep1.js
@@ -1,26 +1,50 @@
 import {useState, useRef} from 'react';
 import './fetchStep1.css';
 
-
 export default function FetchStep1 () {
 
-    let [url, setUrl] = useState("https://latinvocabularytester.com/");
+    const [url, setUrl] = useState("https://latinvocabularytester.com/");
+
     const frame = useRef(null);
 
     async function fetchFrame() {
         console.log("Called");
         const urlMatcher = /(https?:\/\/[^/]*)\//;
 
+        //Can we react-refactor this?
+
         document.cookie = "targetPage="+url+"; SameSite=None; Secure";
         document.cookie = "targetDomain="+url.match(urlMatcher)[1]+"; SameSite=None; Secure";
         frame.current.contentWindow.location.reload(); //forces update
-      
-        // document.querySelectorAll('*').forEach(item => {
-        //   item.addEventListener('click', event => {
-        //     alert("You clicked me")
-        //   })
-        // })  
       }
+
+      function handleLoad() {
+        var css = '.blue {background-color: blue !important; background-image:none !important;}'
+        var style = document.createElement('style');
+        style.appendChild(document.createTextNode(css));
+        frame.current.contentDocument.head.appendChild(style);
+        frame.current.contentDocument.querySelectorAll('*').forEach(mouseIn);
+        frame.current.contentDocument.querySelectorAll('*').forEach(mouseOut);
+      }
+
+      function mouseIn(item) {
+        item.addEventListener('mouseover', event => {
+            console.log(event.target);
+
+            if (typeof  frame.current.contentDocument.body.previous !== 'undefined') {
+                frame.current.contentDocument.body.previous.classList.remove('blue');
+            } 
+            event.target.classList.add('blue');
+            frame.current.contentDocument.body.previous = event.target;
+        })
+      }
+
+      function mouseOut(item) {
+        item.addEventListener('mouseout', event => {
+            event.target.classList.remove('blue');
+        })
+      }
+
 
       function handleChange(e) {
         setUrl(e.target.value);
@@ -30,7 +54,7 @@ export default function FetchStep1 () {
         <>
             <input id="url" type="text" value={url} onChange={handleChange} />
             <input type="button" onClick={fetchFrame} value="Fetch!"/>
-            <iframe ref={frame} id="elementPickFrame" src='./proxyPage' />
+            <iframe ref={frame} id="elementPickFrame" src='./proxyPage' onLoad={handleLoad}/>
         </>
       ) 
       
