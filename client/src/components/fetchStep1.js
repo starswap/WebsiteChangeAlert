3090e83af76125a7c20a8b6ce8bd8d7ac7e31dae
Hamish Starling <hamishstarling@hotmail.co.uk>
2022-09-01 13:25:28 +0100

    Refactor to separate iframe code

--- client/src/components/fetchStep1.js
+++ client/src/components/fetchStep1.js
@@ -1,50 +1,20 @@
-import {useState, useRef} from 'react';
+import {useState, useRef, useEffect} from 'react';
 import './fetchStep1.css';
 
 export default function FetchStep1 () {
 
     const [url, setUrl] = useState("https://latinvocabularytester.com/");
+    const [needsReload,setNeedsReload] = useState(false);
 
     const frame = useRef(null);
 
     async function fetchFrame() {
-        console.log("Called");
         const urlMatcher = /(https?:\/\/[^/]*)\//;
-
         //Can we react-refactor this?
-
         document.cookie = "targetPage="+url+"; SameSite=None; Secure";
         document.cookie = "targetDomain="+url.match(urlMatcher)[1]+"; SameSite=None; Secure";
-        frame.current.contentWindow.location.reload(); //forces update
+        setNeedsReload( (val) => !val);
       }
-
-      function handleLoad() {
-        var css = '.blue {background-color: blue !important; background-image:none !important;}'
-        var style = document.createElement('style');
-        style.appendChild(document.createTextNode(css));
-        frame.current.contentDocument.head.appendChild(style);
-        frame.current.contentDocument.querySelectorAll('*').forEach(mouseIn);
-        frame.current.contentDocument.querySelectorAll('*').forEach(mouseOut);
-      }
-
-      function mouseIn(item) {
-        item.addEventListener('mouseover', event => {
-            console.log(event.target);
-
-            if (typeof  frame.current.contentDocument.body.previous !== 'undefined') {
-                frame.current.contentDocument.body.previous.classList.remove('blue');
-            } 
-            event.target.classList.add('blue');
-            frame.current.contentDocument.body.previous = event.target;
-        })
-      }
-
-      function mouseOut(item) {
-        item.addEventListener('mouseout', event => {
-            event.target.classList.remove('blue');
-        })
-      }
-
 
       function handleChange(e) {
         setUrl(e.target.value);
@@ -54,8 +24,48 @@ export default function FetchStep1 () {
         <>
             <input id="url" type="text" value={url} onChange={handleChange} />
             <input type="button" onClick={fetchFrame} value="Fetch!"/>
-            <iframe ref={frame} id="elementPickFrame" src='./proxyPage' onLoad={handleLoad}/>
+            <MyFrame ref={frame} id="elementPickFrame" src='./proxyPage' needsReload={needsReload}/>
         </>
       ) 
       
+}
+
+function MyFrame(props) {
+    let theFrame = useRef(null);
+
+    //allow parent to reload the frame.
+    useEffect( () => { 
+        if (theFrame.current !== null) {
+            theFrame.current.contentWindow.location.reload();
+        }
+    },[props.needsReload]);
+
+    function handleLoad() {
+        let css = '.blue {background-color: blue !important; background-image:none !important;}'
+        let style = document.createElement('style');
+        style.appendChild(document.createTextNode(css));
+        theFrame.current.contentDocument.head.appendChild(style);
+        theFrame.current.contentDocument.querySelectorAll('*').forEach( (item) =>
+            item.addEventListener('mouseover', mouseIn)
+        );
+        theFrame.current.contentDocument.querySelectorAll('*').forEach( (item) =>
+            item.addEventListener('mouseout', mouseOut)
+        );
+    }
+
+    function mouseIn(event) {
+        console.log(event.target);
+
+        if (typeof theFrame.current.contentDocument.body.previous !== 'undefined') {
+            theFrame.current.contentDocument.body.previous.classList.remove('blue');
+        } 
+        event.target.classList.add('blue');
+        theFrame.current.contentDocument.body.previous = event.target;
+    }
+
+    function mouseOut(event) {
+        event.target.classList.remove('blue');
+    }
+
+    return <iframe ref={theFrame} id={props.id} src={props.src} onLoad={handleLoad} />;
 }
