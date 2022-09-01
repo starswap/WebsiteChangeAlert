fd1cdd1ccfdfcba94b2d566d20f0b443190b09b5
Hamish Starling <hamishstarling@hotmail.co.uk>
2022-09-01 14:48:57 +0100

    Convert to use submit rather than button

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,10 @@ import FetchStep1 from './components/fetchStep1';
 function App() {
   return (
 
-    <>
+    <div class="App">
       <h1 id="title">Welcome to the website change alert.</h1>
       <FetchStep1 />
-    </>
+    </div>
 
   );
 }
--- client/src/components/fetchStep1.js
+++ client/src/components/fetchStep1.js
@@ -3,14 +3,19 @@ import './fetchStep1.css';
 
 export default function FetchStep1 () {
 
-    const [url, setUrl] = useState("https://latinvocabularytester.com/");
+    let [url, setUrl] = useState("https://latinvocabularytester.com/");
     const [needsReload,setNeedsReload] = useState(false);
 
     const frame = useRef(null);
 
-    async function fetchFrame() {
+    async function fetchFrame(e) {
+        e.preventDefault();
         const urlMatcher = /(https?:\/\/[^/]*)\//;
         //Can we react-refactor this?
+        if (url[url.length-1] != '/') {
+            url += '/'; //temporary
+        }
+
         document.cookie = "targetPage="+url+"; SameSite=None; Secure";
         document.cookie = "targetDomain="+url.match(urlMatcher)[1]+"; SameSite=None; Secure";
         setNeedsReload( (val) => !val);
@@ -21,11 +26,11 @@ export default function FetchStep1 () {
       }
 
       return (
-        <>
-            <input id="url" type="text" value={url} onChange={handleChange} />
-            <input type="button" onClick={fetchFrame} value="Fetch!"/>
+        <form onSubmit={fetchFrame}>
+            <input id="url" type="text" value={url} onChange={handleChange} /> 
+            <input type="submit" value="Fetch!"/> <br />
             <MyFrame ref={frame} id="elementPickFrame" src='./proxyPage' needsReload={needsReload}/>
-        </>
+        </form>
       ) 
       
 }
@@ -41,7 +46,7 @@ function MyFrame(props) {
     },[props.needsReload]);
 
     function handleLoad() {
-        let css = '.blue {background-color: blue !important; background-image:none !important;}'
+        let css = '.blue {background-color: blue !important; background-image:none !important;}';
         let style = document.createElement('style');
         style.appendChild(document.createTextNode(css));
         theFrame.current.contentDocument.head.appendChild(style);
@@ -51,10 +56,13 @@ function MyFrame(props) {
         theFrame.current.contentDocument.querySelectorAll('*').forEach( (item) =>
             item.addEventListener('mouseout', mouseOut)
         );
+        theFrame.current.contentDocument.querySelectorAll('*').forEach( (item) =>
+            item.addEventListener('click', handleClick)
+        );        
     }
 
     function mouseIn(event) {
-        console.log(event.target);
+        //console.log(event.target);
 
         if (typeof theFrame.current.contentDocument.body.previous !== 'undefined') {
             theFrame.current.contentDocument.body.previous.classList.remove('blue');
@@ -65,6 +73,11 @@ function MyFrame(props) {
 
     function mouseOut(event) {
         event.target.classList.remove('blue');
+    }
+
+    function handleClick(event) {
+        event.preventDefault();
+        alert(theFrame.current.contentDocument.body.previous);
     }
 
     return <iframe ref={theFrame} id={props.id} src={props.src} onLoad={handleLoad} />;
