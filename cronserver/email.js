6cd36aafb991c18aee9259e88a38f2193ae39eb5
Hamish Starling <hamishstarling@hotmail.co.uk>
2022-09-07 15:09:40 +0100

    Support HTML emails

--- cronserver/email.js
+++ cronserver/email.js
@@ -1,27 +1,43 @@
 import nodemailer from 'nodemailer';
 import {config} from 'dotenv';
+import fs from 'fs';
+
 config();
 
+function sendEmailWithContent(subject,htmlContent) {
+    var transporter = nodemailer.createTransport({
+        service: process.env.EMAIL_SERVICE,
+        auth: {
+          user: process.env.EMAIL_ADDRESS,
+          pass: process.env.EMAIL_PASSWORD
+        }
+      });
+      
+      var mailOptions = {
+        from: process.env.EMAIL_ADDRESS,
+        to: 'hamishstarling@gmail.com',
+        subject: subject,
+        html: htmlContent
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+}
 
-var transporter = nodemailer.createTransport({
-  service: process.env.EMAIL_SERVICE,
-  auth: {
-    user: process.env.EMAIL_ADDRESS,
-    pass: process.env.EMAIL_PASSWORD
-  }
-});
 
-var mailOptions = {
-  from: process.env.EMAIL_ADDRESS,
-  to: 'hamishstarling@gmail.com',
-  subject: 'New Jane Street Puzzle',
-  html: "<body><div style='background-color:blue'>Dear Hamish, A new Jane Street Puzzle is available.</div></body>"
-};
+export function sendEmail() {
+    fs.readFile('email_template.html', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        sendEmailWithContent('Website Change Alert: New Jane Street Puzzle',data)
+    });    
+}
 
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
+sendEmail();
