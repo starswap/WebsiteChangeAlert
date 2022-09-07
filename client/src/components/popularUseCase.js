7ca3e18428df06a9343b4be01787cb9664c558ea
Hamish Starling <hamishstarling@hotmail.co.uk>
2022-09-07 17:32:51 +0100

    Fix wrong prop name

--- client/src/components/popularUseCase.js
+++ client/src/components/popularUseCase.js
@@ -2,7 +2,7 @@ import "./popularUseCase.css";
 
 export default function PopularUseCase(props) {
     return (
-        <div class="column popularUseCase">
+        <div className="column popularUseCase">
             <img alt="A screenshot of a popular use case of this app." className="circleImage" src={props.src} />
             <p>{props.text}</p>
         </div>
