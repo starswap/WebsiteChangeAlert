d1aa16856b30e9047bc200eba16b7dfb52b9be78
Hamish Starling <hamishstarling@hotmail.co.uk>
2022-09-13 18:44:54 +0100

    Added link to XKCD licence

--- client/src/components/processSteps/fetchStep2.js
+++ client/src/components/processSteps/fetchStep2.js
@@ -67,7 +67,7 @@ function LoadIndicator(props) {
             <canvas ref={canvasRef} height={100}></canvas>
             <p>Just loading that page for you; hold on tight.</p>
             <div class="spacer"></div>
-            <a href="https://xkcd.com/612/"><img style={{height:250}} src="https://imgs.xkcd.com/comics/estimation.png" alt="An XKCD comic about progress estimation."/></a>
+            <a href="https://xkcd.com/612/"><img style={{height:250}} src="https://imgs.xkcd.com/comics/estimation.png" alt="An XKCD comic about progress estimation; LICENCE: https://creativecommons.org/licenses/by-nc/2.0/legalcode"/></a>
             <p>(Courtesy of <a id="xkcdLink" href="https://xkcd.com/612/">XKCD</a>)</p>
             <div class="spacer"></div>
         </div>)
