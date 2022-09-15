33ed5ca2e501f309cbcdb3418c9ff917ea505e4c
Hamish Starling <hamishstarling@hotmail.co.uk>
2022-09-15 12:26:00 +0100

    Fix warnings for deployment

--- client/src/components/AboutMe/AboutMe.js
+++ client/src/components/AboutMe/AboutMe.js
@@ -1,4 +1,3 @@
-import smilingClock from '../../assets/smilingClock.png';
 import "./AboutMe.css";
 
 export default function AboutMe() {
@@ -12,11 +11,11 @@ export default function AboutMe() {
                     
                     <div className='spacer'></div>
                     <div className='spacer'></div>
-                    <Social link="https://github.com/starswap" imgLink="https://upload.wikimedia.org/wikipedia/commons/c/cd/GitHub-Mark-64px.png" />
+                    <Social link="https://github.com/starswap" imgLink="https://upload.wikimedia.org/wikipedia/commons/c/cd/GitHub-Mark-64px.png" socialName="Github"/>
                     <div className='spacer'></div>
-                    <Social link="https://www.linkedin.com/in/hamish-starling-147859235/" imgLink="https://brand.linkedin.com/content/dam/me/brand/en-us/brand-home/logos/In-Blue-Logo.png.original.png" />
+                    <Social link="https://www.linkedin.com/in/hamish-starling-147859235/" imgLink="https://brand.linkedin.com/content/dam/me/brand/en-us/brand-home/logos/In-Blue-Logo.png.original.png" socialName="Linkedin"/>
                     <div className='spacer'></div>
-                    <Social link="https://devpost.com/starswap" imgLink="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz48IURPQ1RZUEUgc3ZnIFBVQkxJQyAiLS8vVzNDLy9EVEQgU1ZHIDEuMS8vRU4iICJodHRwOi8vd3d3LnczLm9yZy9HcmFwaGljcy9TVkcvMS4xL0RURC9zdmcxMS5kdGQiPjxzdmcgdmVyc2lvbj0iMS4xIiBpZD0iTGF5ZXJfMSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxuczp4bGluaz0iaHR0cDovL3d3dy53My5vcmcvMTk5OS94bGluayIgeD0iMHB4IiB5PSIwcHgiIHZpZXdCb3g9IjAgMCAyODAuMyAyNDIiIGVuYWJsZS1iYWNrZ3JvdW5kPSJuZXcgMCAwIDI4MC4zIDI0MiIgeG1sOnNwYWNlPSJwcmVzZXJ2ZSI+PGcgaWQ9IlhNTElEXzFfIj48cGF0aCBpZD0iWE1MSURfNl8iIGQ9Ik0xMzMuNyw3NkgxMTh2OTBoMTQuN2MzMC45LDAsNDUuMS0xOC4xLDQ1LjEtNDVDMTc3LjgsOTAuOSwxNjQuOSw3NiwxMzMuNyw3NnoiLz48cGF0aCBpZD0iWE1MSURfOV8iIGQ9Ik0yMTAuMiwwSDcwLjFMMCwxMjFsNzAuMSwxMjFoMTQwLjFsNzAuMS0xMjFMMjEwLjIsMHogTTEzMi43LDE5NUg4OVY0N2g0NS44YzQyLjEsMCw3My4zLDIwLjEsNzMuMyw3NEMyMDguMSwxNzIuOCwxNzAuNiwxOTUsMTMyLjcsMTk1eiIvPjwvZz48L3N2Zz4=" />
+                    <Social link="https://devpost.com/starswap" imgLink="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz48IURPQ1RZUEUgc3ZnIFBVQkxJQyAiLS8vVzNDLy9EVEQgU1ZHIDEuMS8vRU4iICJodHRwOi8vd3d3LnczLm9yZy9HcmFwaGljcy9TVkcvMS4xL0RURC9zdmcxMS5kdGQiPjxzdmcgdmVyc2lvbj0iMS4xIiBpZD0iTGF5ZXJfMSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxuczp4bGluaz0iaHR0cDovL3d3dy53My5vcmcvMTk5OS94bGluayIgeD0iMHB4IiB5PSIwcHgiIHZpZXdCb3g9IjAgMCAyODAuMyAyNDIiIGVuYWJsZS1iYWNrZ3JvdW5kPSJuZXcgMCAwIDI4MC4zIDI0MiIgeG1sOnNwYWNlPSJwcmVzZXJ2ZSI+PGcgaWQ9IlhNTElEXzFfIj48cGF0aCBpZD0iWE1MSURfNl8iIGQ9Ik0xMzMuNyw3NkgxMTh2OTBoMTQuN2MzMC45LDAsNDUuMS0xOC4xLDQ1LjEtNDVDMTc3LjgsOTAuOSwxNjQuOSw3NiwxMzMuNyw3NnoiLz48cGF0aCBpZD0iWE1MSURfOV8iIGQ9Ik0yMTAuMiwwSDcwLjFMMCwxMjFsNzAuMSwxMjFoMTQwLjFsNzAuMS0xMjFMMjEwLjIsMHogTTEzMi43LDE5NUg4OVY0N2g0NS44YzQyLjEsMCw3My4zLDIwLjEsNzMuMyw3NEMyMDguMSwxNzIuOCwxNzAuNiwxOTUsMTMyLjcsMTk1eiIvPjwvZz48L3N2Zz4=" socialName="Devpost"/>
                     <div className='spacer'></div>
                     <div className='spacer'></div>
 
@@ -24,7 +23,7 @@ export default function AboutMe() {
             </div>
 
             <div className='column'>
-                <img id="me" src="https://avatars.githubusercontent.com/u/37508609?v=4" alt=""/>
+                <img id="me" src="https://avatars.githubusercontent.com/u/37508609?v=4" alt="the creator of this website"/>
             </div>
 
         </div>
@@ -34,7 +33,7 @@ export default function AboutMe() {
 function Social(props) {
     return (
         <a href={props.link} target="__blank" className='surroundingButton'>
-            <img src={props.imgLink} />
+            <img src={props.imgLink} alt={props.socialName}/>
         </a>
     )
 }
--- client/src/components/BottomBar/BottomBar.js
+++ client/src/components/BottomBar/BottomBar.js
@@ -5,12 +5,12 @@ export default function BottomBar() {
         <div className="row flexCenter">
             <p id="madeWithText">Made with: </p>
 
-            <img className="techStackImage" src="https://webimages.mongodb.com/_com_assets/cms/kusb9stg1ndrp7j53-MongoDBLogoBrand1.png?auto=format%252Ccompress" width="100px"/>
+            <img alt="MongoDB Logo" className="techStackImage" src="https://webimages.mongodb.com/_com_assets/cms/kusb9stg1ndrp7j53-MongoDBLogoBrand1.png?auto=format%252Ccompress" width="100px"/>
 
-            <img className="techStackImage" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/590px-Node.js_logo.svg.png?20170401104355" width="100px"/>
+            <img alt="NodeJS Logo" className="techStackImage" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/590px-Node.js_logo.svg.png?20170401104355" width="100px"/>
 
-            <img className="techStackImage" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png?20220125121207" width="50px"/>
-            <img className="techStackImage" src="https://www.vectorlogo.zone/logos/expressjs/expressjs-ar21.svg" width="100px"/>
+            <img alt="ReactJS Logo" className="techStackImage" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png?20220125121207" width="50px"/>
+            <img alt="ExpressJS Logo" className="techStackImage" src="https://www.vectorlogo.zone/logos/expressjs/expressjs-ar21.svg" width="100px"/>
         </div>
         <div className="row flexCenter">
             <p id="openSourcePara">This project is proudly <a target="__blank" href="https://github.com/starswap/WebsiteChangeAlert" id="openSourceLink">Open Source</a>.</p>
