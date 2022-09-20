a17f9acd042a3b7849f10a4056a1a274adb7ea10
Hamish Starling <hamishstarling@hotmail.co.uk>
2022-09-20 18:44:57 +0100

    Require password authentication to create an alert

--- client/src/components/processSteps/fetchStep5.js
+++ client/src/components/processSteps/fetchStep5.js
@@ -1,9 +1,19 @@
 import './fetchStep5.css';
 
 export default function FetchStep5 (props) {
-    return (<>
+    if (props.success) {
+        return (<>
             <h3 style={{marginBottom: 0}}>Done!</h3>
             <p>We will notify you if {props.url} changes.</p>
             <input type="button" id="another" value="Make Another Alert!" onClick={props.reset}/>
         </>
-   )}
+        )
+    } else {
+        return (<>
+            <h3 style={{marginBottom: 0}}>Failure!</h3>
+            <p>I'm afraid you need to authenticate to get access to the service; please contact the Administrator.</p>
+            <input type="button" id="another" value="Go Back Home" onClick={props.reset}/>
+        </>
+        )
+    }
+}
